672c395a45fde8648b9e3e2a0cbb8f817756010a
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2026-04-20 15:41:58 +0200

    More attempts to make iOS things visible

--- composeJournalsApp/build.gradle.kts
+++ composeJournalsApp/build.gradle.kts
@@ -38,6 +38,7 @@ kotlin {
             //isStatic = true
             export(projects.shared)
             linkerOpts.add("-lsqlite3")
+            freeCompilerArgs += listOf("-Xbinary=bundleId=at.techbee.spectacled.journals")
         }
     }
 
--- composeNotesApp/build.gradle.kts
+++ composeNotesApp/build.gradle.kts
@@ -39,6 +39,7 @@ kotlin {
             //isStatic = true
             export(projects.shared)
             linkerOpts.add("-lsqlite3")
+            freeCompilerArgs += listOf("-Xbinary=bundleId=at.techbee.spectacled.notes")
         }
     }
 
