bde4e411ac747e77134e8c2627be992a9c6a8458
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2026-06-01 11:26:11 +0200

    Updated Android db driver initialization to reduce risk of problems.

--- shared/src/androidMain/kotlin/at/techbee/spectacled/screens/core/DatabaseDriverFactory.android.kt
+++ shared/src/androidMain/kotlin/at/techbee/spectacled/screens/core/DatabaseDriverFactory.android.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.sqlite.db.SupportSQLiteDatabase
 import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.QueryResult
-import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.db.SqlSchema
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import at.techbee.spectacled.SpectacledVariant
@@ -19,33 +18,34 @@ actual class DatabaseDriverFactory(
 
     companion object {
         private val mutex = Mutex()
-        private var database: SpectacledDatabase? = null
+        private val databases = mutableMapOf<String, SpectacledDatabase>()
     }
 
     actual suspend fun provideDatabase(
         schema: SqlSchema<QueryResult.AsyncValue<Unit>>
     ): SpectacledDatabase {
         return mutex.withLock {
-            database?: SpectacledDatabase(AndroidSqliteDriver(
-                schema = schema.synchronous(),
-                context = context.applicationContext,
-                name = spectacledVariant.dbName,
-                callback = object : AndroidSqliteDriver.Callback(schema.synchronous()) {
+            databases.getOrPut(spectacledVariant.dbName) {
+                SpectacledDatabase(AndroidSqliteDriver(
+                    schema = schema.synchronous(),
+                    context = context.applicationContext,
+                    name = spectacledVariant.dbName,
+                    callback = object : AndroidSqliteDriver.Callback(schema.synchronous()) {
 
-                    override fun onConfigure(db: SupportSQLiteDatabase) {
-                        super.onConfigure(db)
-                        db.setForeignKeyConstraintsEnabled(true)  // also enables cascading delete
-                    }
+                        override fun onConfigure(db: SupportSQLiteDatabase) {
+                            super.onConfigure(db)
+                            db.setForeignKeyConstraintsEnabled(true)  // also enables cascading delete
+                        }
 
-                    override fun onOpen(db: SupportSQLiteDatabase) {
-                        super.onOpen(db)
-                        db.enableWriteAheadLogging()
+                        override fun onOpen(db: SupportSQLiteDatabase) {
+                            super.onOpen(db)
+                            db.enableWriteAheadLogging()
+                        }
                     }
+                )).also { db ->
+                    // 🔥 FORCE DB OPEN HERE
+                    db.calendar_dtoQueries.getAllCalendars().executeAsList()
                 }
-            )).also { db ->
-                // 🔥 FORCE DB OPEN HERE
-                db.calendar_dtoQueries.getAllCalendars().executeAsList()
-                database = db
             }
         }
     }
