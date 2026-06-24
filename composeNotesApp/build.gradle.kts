18eb3258016fa2bf21946dab59ebd3ea002b6f49
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2026-06-24 14:39:47 +0200

    Added freeCompilerArgs for bundleId to remove warning from iOS build

--- composeNotesApp/build.gradle.kts
+++ composeNotesApp/build.gradle.kts
@@ -37,7 +37,7 @@ kotlin {
             //isStatic = true
             export(projects.shared)
             linkerOpts.add("-lsqlite3")
-            //freeCompilerArgs += listOf("-Xbinary=bundleId=at.techbee.spectacled.notes")
+            freeCompilerArgs += listOf("-Xbinary=bundleId=at.techbee.spectacled.notes.ios")
         }
     }
 
