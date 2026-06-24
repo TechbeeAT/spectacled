6e0deaa91acdbac9270d88e66bee234f8139e3d0
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2026-06-24 09:56:28 +0200

    init Koin in AppDelegate to make sure it also runs when the process is started in the background

--- iosJournalsApp/iosJournalsApp/AppDelegate.swift
+++ iosJournalsApp/iosJournalsApp/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
+        KoinIOSKt.initKoinIos(spectacledVariant: .journals)
         log.info("App did finish launching")
 
         // Registration must happen before didFinishLaunchingWithOptions returns
