a9f565dd6e2553f403823e5239d1dfc1efce4b43
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2026-07-11 15:14:08 +0200

    cleaned up imports

--- shared/src/commonMain/kotlin/at/techbee/spectacled/screens/account/presentation/components/SettingsBottomSheet.kt
+++ shared/src/commonMain/kotlin/at/techbee/spectacled/screens/account/presentation/components/SettingsBottomSheet.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.text.input.TextObfuscationMode
 import androidx.compose.foundation.text.input.rememberTextFieldState
