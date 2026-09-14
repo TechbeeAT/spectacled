9ef32daea3ca4e5dbb9771533b4e9b1cce174602
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2026-09-14 12:05:01 +0200

    Added IME action search to search bar and freed focus when search is clicked

--- shared/src/commonMain/kotlin/at/techbee/spectacled/screens/list/presentation/components/ListSearchBar.kt
+++ shared/src/commonMain/kotlin/at/techbee/spectacled/screens/list/presentation/components/ListSearchBar.kt
@@ -3,6 +3,8 @@ package at.techbee.spectacled.screens.list.presentation.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -11,6 +13,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import at.techbee.spectacled.screens.list.presentation.ListAction
@@ -28,12 +33,21 @@ fun ListSearchBar(
     searchBarFocusRequester: FocusRequester = remember { FocusRequester() }
 ) {
 
+    val focusManager = LocalFocusManager.current
+
     Column(modifier = modifier) {
 
         TextField(
             placeholder = { Text(stringResource(Res.string.search)) },
             value = listFilterCriteria.searchQuery ?: "",
             onValueChange = { onAction(ListAction.OnListFilterCriteriaChanged(listFilterCriteria.copy(searchQuery = it))) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Filter,
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = { focusManager.clearFocus() }
+            ),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = 8.dp)
